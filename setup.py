68e13ae3a25856dfdf3f16eab7b108e61a20a149
Sergey Yaroslavtsev <sergey.yaroslavtsev@esrf.fr>
2025-07-04 21:41:14 +0200

    23th

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extensions = [
 
 setup(
     name="cy_py_test",
-    version="0.2.0",
+    version="0.2.1",
     author="Void",
     description="A minimal Cython + Python project for testing",
     ext_modules=cythonize(extensions),
