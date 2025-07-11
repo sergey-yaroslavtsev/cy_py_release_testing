54b18a3ac4271372a3506c1fc4b76e49ab006d72
Sergey Yaroslavtsev <sergey.yaroslavtsev@esrf.fr>
2025-07-11 18:11:07 +0200

    79th MacOS

--- conda/build.sh
+++ conda/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-$PYTHON -m pip install --no-deps --no-build-isolation *.whl
+$PYTHON -m pip install --no-deps --no-build-isolation https://test-files.pythonhosted.org/packages/f1/05/6df8e65f222b5ab3579725999a6217f77ff2cfb37bd5429f316a9fe81160/cy_py_test-0.3.3-cp311-cp311-macosx_10_9_universal2.whl
+
